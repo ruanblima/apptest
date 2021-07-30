15d84d64245d11daced36d219cb1d9048c4f26fd
Ruan Lima <ruan.lima@dellead.com>
2021-07-30 11:29:37 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,98 @@
+import React, {useEffect, useState, useRef} from 'react';
+import {Alert} from 'react-native';
+import * as UpdateAPK from 'rn-update-apk';
+
+import * as S from './styles';
+
+const Home = () => {
+  const [downloadProgress, setDownloadProgress] = useState(0);
+
+  const updater = useRef(null);
+
+  const setUpdater = () => {
+    updater.current = new UpdateAPK.UpdateAPK({
+      iosAppId: '1104809018',
+      apkVersionUrl:
+        'https://raw.githubusercontent.com/mikehardy/react-native-update-apk/master/example/test-version.json',
+
+      apkVersionOptions: {
+        method: 'GET',
+        headers: {},
+      },
+
+      apkOptions: {
+        headers: {},
+      },
+
+      fileProviderAuthority: 'com.apptest.provider',
+
+      needUpdateApp: performUpdate => {
+        Alert.alert(
+          'Update Available',
+          'New version released, do you want to update? ' +
+            '(TESTING NOTE 1: stop your dev package server now - or the test package will try to load from it ' +
+            'instead of the included bundle leading to Javascript/Native incompatibilities.' +
+            'TESTING NOTE 2: the version is fixed at 1.0 so example test updates always work. ' +
+            'Compare the Last Update Times to verify it installed)',
+          [
+            {text: 'Cancel', onPress: () => {}},
+
+            {text: 'Update', onPress: () => performUpdate(true)},
+            ,
+          ],
+        );
+      },
+
+      forceUpdateApp: () => {
+        console.log('forceUpdateApp callback called');
+      },
+
+      notNeedUpdateApp: () => {
+        console.log('notNeedUpdateApp callback called');
+      },
+
+      downloadApkStart: () => {
+        console.log('downloadApkStart callback called');
+      },
+
+      downloadApkProgress: progress => {
+        console.log(`downloadApkProgress callback called - ${progress}%...`);
+
+        setDownloadProgress(progress);
+      },
+
+      downloadApkEnd: () => {
+        console.log('downloadApkEnd callback called');
+      },
+
+      onError: err => {
+        console.log('onError callback called', err);
+        Alert.alert('There was an error', err.message);
+      },
+    });
+  };
+
+  useEffect(() => {
+    setUpdater();
+  }, []);
+
+  const _onCheckServerVersion = () => {
+    console.log('checking for update');
+    updater.current.checkUpdate();
+  };
+  return (
+    <S.Container>
+      <S.Text>Aplicação 1.0</S.Text>
+
+      <S.ButtonDownload onPress={() => _onCheckServerVersion()}>
+        <S.ButtonText>Atualizar APP</S.ButtonText>
+      </S.ButtonDownload>
+
+      {downloadProgress !== -1 && (
+        <S.Text>Download Progress: {downloadProgress}%</S.Text>
+      )}
+    </S.Container>
+  );
+};
+
+export default Home;
---
+++ src/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Text = styled.Text`
+  font-size: 30px;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 20px;
+  color: white;
+`;
+
+export const ButtonDownload = styled.TouchableOpacity`
+  background-color: #0076ce;
+  padding: 15px;
+  border-radius: 6px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
